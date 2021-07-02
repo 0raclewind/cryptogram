fce0ded3ee1ba57b0f720e62d6ced31ff14c7166
0raclewind <look4award@yahoo.com>
2021-07-02 17:13:30 +0100

    Fixed environment variables issues

--- cryptogram/settings.py
+++ cryptogram/settings.py
@@ -83,8 +83,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': env('DB_NAME'),
-        'USER': env('USER'),
-        'PASSWORD': env('PASSWORD'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
         'HOST': env('HOST'),
         'PORT': env('PORT'),
     }

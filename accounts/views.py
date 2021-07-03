e99734c3cbec733f6a2aeb389d425f06480dbee9
0raclewind <look4award@yahoo.com>
2021-07-03 07:25:38 +0100

    Development staging

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,27 @@
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+
 from django.shortcuts import render
+
+from .forms import UserForm
 
 def index(request):
     return render(request, "index.html")
 
 def login(request):
-    return render(request, "login.html")
+    if request.method == "POST":
+        username = request.POST("username")
+        password = request.POST("password")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse('index'))
+    return render(request, "login.html", {
+        "userForm": UserForm()
+    })
+
+def register(request):
+    pass

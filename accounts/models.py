dc77ded0ed8ebd11b9cc2dec5db86514734add0c
0raclewind <look4award@yahoo.com>
2021-08-24 08:53:31 +0100

    Changed max_length to 20 for symbol and slug fields

--- accounts/models.py
+++ accounts/models.py
@@ -7,8 +7,8 @@ class User(AbstractUser):
 
 class Portfolio(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    symbol = models.CharField(max_length=10)
-    slug = models.CharField(max_length=10, null=True)
+    symbol = models.CharField(max_length=20)
+    slug = models.CharField(max_length=20, null=True)
     name = models.CharField(max_length=30)
     amount = models.DecimalField(max_digits=20, decimal_places=7)
 
